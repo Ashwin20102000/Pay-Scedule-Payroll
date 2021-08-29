0fee8d638b4f85e2fbcd4a317ca8c40f24b0d5dd
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2021-08-29 14:07:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form';
 export default function App() {
   const refer = useRef();
   const { register, handleSubmit } = useForm();
-  const weekDay = [
+  let weekDay = [
     { day: 'Monday', Check: false },
     { day: 'Tuesday', Check: false },
     { day: 'Wednesday', Check: false },
@@ -14,9 +14,21 @@ export default function App() {
     { day: 'Saturday', Check: false },
     { day: 'Sunday', Check: false }
   ];
+  let setDate=''
   const onSubmit = data => {
-    const updatedDay = refer.current.value;
-    data.day = updatedDay;
+    let weekDays = [];
+    const updatedDay = weekDay.filter(day => {
+      if (day.Check === true) weekDays.push(day.day);
+    });
+    data.day = weekDays;
+    if (
+      data.hoursWork === '' ||
+      data.payDate === '' ||
+      data.payDateFrom == ''
+    ) {
+      alert('Fill all the Details');
+      return;
+    }
     const updateDate = data.payDate
       .split('-')
       .reverse()
@@ -27,30 +39,34 @@ export default function App() {
       .reverse()
       .join('-');
     data.payDateFrom = updateDateFrom;
-    console.log(data);
-  };
-  const AddDay = day => {
-    weekDay[day].Check = !weekDay[day].Check;
-    console.log(weekDay[day].Check);
+    setDate=data.payDateFrom
+    // console.log(data);
+    return data
   };
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="workWeek">Select Work Week </label>
+      <h1 className="text-light bg-dark p-4 ">Pay Schedule</h1>
+      <form className="form-group m-5 p-3 px-6" onSubmit={handleSubmit(onSubmit)}>
+        <h2>It's Time to set up your Pay Schedule</h2>
+        <small class="form-text text-muted">
+          tell us when and how when you pay your employees. We'll make they will
+          be paid on time
+        </small>
+        <label className="mt-4 mb-2" htmlFor="workWeek">Select Work Week </label>
+        <br />
         {weekDay.map((day, i) => {
           return (
             <>
               <br />
-              <label>
-                <input
-                  key={day.day}
+              <input class="form-check-input" key={day.day}
                   type="checkbox"
-                  onChange={AddDay(i)}
+                  htmlFor="day"
+                  onChange={() => {
+                    weekDay[i].Check = !weekDay[i].Check;
+                  }}
                   name={{ day }}
-                  value={{ day }}
-                />
-                {day.day}
-              </label>
+                  value={{ day }} />
+                  <label class="form-check-label" for="day" > &nbsp; {day.day}</label>
             </>
           );
         })}
@@ -58,22 +74,40 @@ export default function App() {
         <br />
         <label htmlFor="hoursWork">Working Hours </label>
         <input
+          className="form-control"
           {...register('hoursWork')}
           type="hours"
           placeholder="Enter Week Hours"
           name="hoursWork"
         />
         <br />
-        <br />
         <label htmlFor="payDate">Pay Date </label>
-        <input {...register('payDate')} type="date" name="payDate" />
-        <br />
+        <input
+          className="form-control"
+          {...register('payDate')}
+          type="date"
+          name="payDate"
+        />
         <br />
         <label htmlFor="payDateFrom">Pay Date From </label>
-        <input {...register('payDateFrom')} type="date" name="payDateFrom" />
+        <input
+          className="form-control"
+          {...register('payDateFrom')}
+          type="date"
+          name="payDateFrom"
+        />
         <br />
-        <br />
-        <input type="submit" value="Submit" />
+        {/* <span class="bg-secondary text-light p-1 rounded ">
+          Salary for the month of {setDate}
+        </span> */}
+        {/* <br />
+        <br /> */}
+        <button
+          className="btn btn-primary"
+          type="submit"
+          value="Save & Continue →"
+        >Save & Continue →
+        </button>
       </form>
     </div>
   );
